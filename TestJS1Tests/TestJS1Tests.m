799e947eee2b2d6071cf757af08b70d79316b1fd
chenhao <554430962@qq.com>
2018-01-24 15:34:34 +0800

    Initial Commit

---
+++ TestJS1Tests/TestJS1Tests.m
@@ -0,0 +1,39 @@
+//
+//  TestJS1Tests.m
+//  TestJS1Tests
+//
+//  Created by administrator on 2018/1/24.
+//  Copyright © 2018年 administrator. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestJS1Tests : XCTestCase
+
+@end
+
+@implementation TestJS1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
